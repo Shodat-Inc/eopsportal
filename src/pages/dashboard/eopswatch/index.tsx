b13617d1701b1e657884022e9279903bdf5516b1
amit pandey <amitpandey0107@gmail.com>
2023-10-12 19:19:28 +0530

    Created Seperate Component for Dropdowns

--- src/pages/dashboard/eopswatch/index.tsx
+++ src/pages/dashboard/eopswatch/index.tsx
@@ -7,6 +7,7 @@ import Table from "./table";
 import Filter from "./filters";
 import axios from "axios";
 import Drop from "./drop";
+import CustomDrop from "@/common/customdrop";
 
 const classData = [
     "Vehicles",
@@ -20,43 +21,15 @@ const classData = [
 export default function EopsWatch() {
 
     const [toggleAsset, setToggleAsset] = useState(false);
-    const [showHideTab, setShowHideTab] = useState(true);
     const [chooseAsset, setChooseAsset] = useState('Manufacturing Plants');
     const [toggleSort, setToggleSort] = useState(false);
 
     const sortByID = () => {
         setToggleSort(!toggleSort)
     }
-
-    // Hook that alerts clicks outside of the passed ref
-    function useOutsideAlerter(ref: any) {
-        useEffect(() => {
-            function handleClickOutside(event: any) {
-                if (ref.current && !ref.current.contains(event.target)) {
-                    setToggleAsset(false)
-                }
-            }
-            // Bind the event listener
-            document.addEventListener("mousedown", handleClickOutside);
-            return () => {
-                // Unbind the event listener on clean up
-                document.removeEventListener("mousedown", handleClickOutside);
-            };
-        }, [ref]);
-    }
-
-    const wrapperRef = useRef(null);
-    useOutsideAlerter(wrapperRef);
-
-    // Show Choose Asset List
-    const showChooseAssetList = () => {
-        setToggleAsset(!toggleAsset)
-    }
-
-    const selectAsset = (item: any) => {
-        setChooseAsset(item);
-        setToggleAsset(false);
-        setShowHideTab(true);
+   
+    const handleDropDown = (item: any) => {
+        setChooseAsset(item)
     }
 
     return (
@@ -66,38 +39,10 @@ export default function EopsWatch() {
             {/* Top Information */}
             <div className="bg-white rounded rounded-xl min-h-[120px] px-3 py-4 flex justify-between items-center mb-4">
                 <div className="w-[34%] border border-[#EEEEEE] border-l-0 border-t-0 border-b-0 border-r-1">
-                    <div
-                        className="border rounded-xl border-gray-500 h-[55px] pl-2 pr-5 relative flex items-center justify-start bg-white w-[80%]"
-                        onClick={showChooseAssetList}
-                    >
-                        <label className="absolute text-sm top-[-10px] left-2 pl-2 pr-2 bg-white">Choose Industry type</label>
-                        <Image
-                            src="/img/arrow-down-black.svg"
-                            alt="arrow-down"
-                            height={20}
-                            width={20}
-                            className="absolute right-3 top-4"
-                        />
-                        <span className="text-lg text-black pl-2">{chooseAsset}</span>
-                    </div>
-
-                    {toggleAsset ?
-                        <div ref={wrapperRef} className={`h-52 border rounded-xl border-gray-500 h-auto max-h-[250px] w-[400px]  absolute flex items-start justify-start mt-1 overflow-hidden overflow-y-auto bg-white ${styles.scroll} z-10`}>
-                            <ul className="p-0 m-0 w-full">
-                                {
-                                    classData.map((item: any, index: any) => (
-                                        <li
-                                            className="px-5 py-2 bg-white cursor-pointer hover:bg-yellow-951 w-full font-normal"
-                                            onClick={() => selectAsset(item)}
-                                            key={index}
-                                        >
-                                            <span>{item}</span>
-                                        </li>
-                                    ))
-                                }
-                            </ul>
-                        </div>
-                        : null}
+                    <CustomDrop
+                        data={classData}
+                        handleClick={handleDropDown}
+                    />
                 </div>
                 <div className="w-[34%] border border-[#EEEEEE] border-l-0 border-t-0 border-b-0 border-r-1">
                     <div className="text-center flex justify-center items-center w-full flex-wrap flex-col">
