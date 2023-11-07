e7849a3d74f89edfe6a97a7506fcdf72f440c270
amitshodat <amitpandey@shodat.com>
2023-11-08 01:20:13 +0530

    Made actions to search and filters in AI Model Detection flow

--- src/pages/dashboard/aimodaldetection/eopstrace.tsx
+++ src/pages/dashboard/aimodaldetection/eopstrace.tsx
@@ -100,63 +100,57 @@ export default function EopsTrace(props: any) {
                 </div>
             </div>
             <div className="w-[80%] px-8 py-5 rounded-r-xl rounded-bl-xl border border-[#E3E3E3] border-l-2 border-t-0 border-b-2 border-r-0 bg-white min-h-[500px]">
-                <div className="flex justify-between items-start mb-8">
-                    <div className="text-md font-semibold">Model Details1</div>
-                    {
-                        data.name === "Crack Detection" &&
-                        <button className="flex justify-center items-center rounded rounded-xl h-[44px] px-1 py-1 bg-black border border-black text-white">
+                <div className='relative'>
+                    <div className="flex justify-start items-start mb-4">
+                        <button
+                            className="h-[44px] px-2 py-1 px-3 flex justify-center items-center bg-yellow-951 border border-yellow-951 text-sm text-black rounded rounded-xl mr-6"
+                        >
+                            <span>Run this model</span>
+                        </button>
+                        <button className="flex justify-center items-center rounded rounded-xl h-[44px] px-1 py-1 bg-[#404040] border border-[#404040] text-white text-sm">
                             <Image
                                 src="/img/activity-white.svg"
                                 alt="activity"
                                 height={20}
                                 width={20}
                             />
-                            <span className="ml-2 text-sm">Model Performance</span>
+                            <span className="ml-2">Model Performance</span>
                         </button>
-                    }
-                </div>
-                <div className="mb-8 flex justify-start item-center">
+                    </div>
+                    <div className="text-md font-semibold mb-4">Model Details</div>
+                    <div className="mb-10 flex justify-start item-center">
+                        <div className="ml-0">
+                            <h3 className="font-semibold text-xl text-[#666666] mb-4">{data.name}</h3>
+                            <p className="font-semibold text-md text-[#666666] mb-1">How it works</p>
+                            <p className="text-[14px] text-[#666666]">{data.description}</p>
+                        </div>
+                    </div>
                     <div className="relative">
-                        <Image
-                            src={`/img/${data.image}`}
-                            alt="crack-detection-large"
-                            height={136}
-                            width={152}
-                        />
-                    </div>
-                    <div className="ml-8">
-                        <h3 className="font-semibold text-xl text-black mb-4">{data.name}</h3>
-                        <p className="font-semibold text-md text-black mb-1">How it works</p>
-                        <p className="text-[14px] text-black">{data.description}</p>
-                    </div>
-                </div>
-                <div className="mb-8">
-                    <button
-                        className="h-[44px] px-2 py-1 px-3 flex justify-center items-center bg-yellow-951 border border-yellow-951 text-md text-black rounded rounded-xl"
-                    >
-                        <span>Access Data</span>
-                    </button>
-                </div>
-                <div className="">
-                    <div className="flex justify-start items-start flex-wrap flex-col mb-8">
-                        <p className="font-semibold text-md text-black mb-2">Benifits</p>
-                        <ul className="text-sm flex flex-between items-center flex-wrap flex-row list-disc pl-5">
-                            {
-                                data.benifits.map((item: any, index: any) => (
-                                    <li key={index} className="w-1/2 mb-3">{item}</li>
-                                ))
-                            }
-                        </ul>
+                        <div className="flex justify-start items-center">
+                            <div className="relative">
+                                <Image
+                                    src={`/img/${data.image}`}
+                                    alt="crack-detection-large"
+                                    height={136}
+                                    width={152}
+                                />
+                            </div>
+                            <div className='ml-10'>
+                                <p className="font-semibold text-md text-black mb-2">Benifits</p>
+                                <ul className="text-sm flex flex-between items-center flex-wrap flex-row list-disc pl-5">
+                                    {
+                                        data.benifits.map((item: any, index: any) => (
+                                            <li key={index} className="w-1/2 mb-3">{item}</li>
+                                        ))
+                                    }
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                     {data.activePlan == true ?
-                        <>
-                            <div className="flex justify-start items-center">
-                                <button
-                                    className="h-[44px] px-2 py-1 flex justify-center items-center bg-yellow-951 border border-yellow-951 text-sm text-black rounded rounded-xl"
-                                >
-                                    <span>Upgrade Plan</span>
-                                </button>
-                                <div className="flex items-center justify-start ml-7">
+                        <div className='bg-[#F2F2F2] w-[290px] h-[104px] flex justify-start items-center p-3 flex-wrap flex-row rounded rounded-lg absolute top-[-12px] right-[-20px]'>
+                            <div className="flex justify-end items-center w-full">
+                                <div className="flex justify-end items-center w-full">
                                     <p className="text-gray mr-3 text-sm">Auto renew</p>
                                     <div className={`${styles.radioWrap} relative top-[2px]`}>
                                         <input
@@ -168,9 +162,9 @@ export default function EopsTrace(props: any) {
                             </div>
 
                             <div className="flex justify-start items-start font-semibold mt-3 text-[13px] text-[#666666]">Your model will be expire on : {data.planExpire}</div>
-                        </>
+                        </div>
                         :
-                        <div className="flex justify-start items-start font-semibold mt-3 text-[13px]">
+                        <div className="flex justify-start items-start font-semibold mt-3 text-[13px] bg-[#F2F2F2] min-w-[290px] h-[104px] flex justify-start items-center p-3 flex-wrap flex-row rounded rounded-lg absolute top-[-12px] right-[-20px]">
                             <button className="mr-5 flex justify-center items-center rounded rounded-xl h-[44px] px-3 py-1 bg-yellow-951 border border-yellow-951">
                                 <Image
                                     src="/img/shopping-cart.svg"
