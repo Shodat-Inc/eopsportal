e6450dbdfa8ac849c9a5a3a57e0224d205bd97e9
amitshodat <amitpandey@shodat.com>
2023-11-16 22:34:59 +0530

    Added delete modal for class and object

--- src/pages/dashboard/assetmanagement/classmanagement.tsx
+++ src/pages/dashboard/assetmanagement/classmanagement.tsx
@@ -10,7 +10,8 @@ export default function ClassManagement(props: any) {
     const [actions, setActions] = useState(false);
     const [actionCount, setActionCount] = useState(1);
     const [showModal, setShowModal] = useState(Boolean);
-    useEffect(()=> {
+    const [deleteModal, setDeleteModal] = useState(false);
+    useEffect(() => {
         setShowModal(props.addClassModal)
     }, [props.addClassModal])
 
@@ -28,11 +29,20 @@ export default function ClassManagement(props: any) {
     const selectedAction = (item: any) => {
         setActions(false);
     }
-    
-    const handleClick = (item:any) => {
+
+    const handleClick = (item: any) => {
         setShowModal(false);
         props.handleaddClassModal(item)
-    } 
+    }
+
+    const deleteModalFunction = () => {
+        setDeleteModal(true);
+        setActions(false);
+    }
+
+    const closeDeleteModal = () => {
+        setDeleteModal(false)
+    }
     return (
         <div className='px-0 py-3 font-OpenSans'>
             {/* Title, search and filters */}
@@ -120,11 +130,11 @@ export default function ClassManagement(props: any) {
                                                 className="text-white text-[14px] hover:bg-yellow-951 hover:text-black h-[30px] px-4 border-b border-gray-900 w-full text-left flex items-center justify-start">
                                                 <span>Edit</span>
                                             </Link>
-                                            <Link
-                                                href="#"
+                                            <button
+                                                onClick={deleteModalFunction}
                                                 className="text-white text-[14px] hover:bg-yellow-951 hover:text-black h-[30px] px-4 border-b border-gray-900 w-full text-left flex items-center justify-start">
                                                 <span>Delete</span>
-                                            </Link>
+                                            </button>
                                         </div>
                                     }
                                 </div>
@@ -153,11 +163,11 @@ export default function ClassManagement(props: any) {
                                                 className="text-white text-[14px] hover:bg-yellow-951 hover:text-black h-[30px] px-4 border-b border-gray-900 w-full text-left flex items-center justify-start">
                                                 <span>Edit</span>
                                             </Link>
-                                            <Link
-                                                href="#"
+                                            <button
+                                                onClick={deleteModalFunction}
                                                 className="text-white text-[14px] hover:bg-yellow-951 hover:text-black h-[30px] px-4 border-b border-gray-900 w-full text-left flex items-center justify-start">
                                                 <span>Delete</span>
-                                            </Link>
+                                            </button>
                                         </div>
                                     }
                                 </div>
@@ -169,9 +179,61 @@ export default function ClassManagement(props: any) {
             </div>
 
 
-            {/* Add New Class */}            
-            <AddNewClass handleClick={handleClick} show={showModal} /> 
-            
+            {/* Add New Class */}
+            <AddNewClass handleClick={handleClick} show={showModal} />
+
+
+            {/* Delete Modal */}
+            {deleteModal &&
+                <>
+                    <div
+                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                    >
+                        <div className="relative my-6 w-[580px]">
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                {/*header*/}
+                                <div className="flex items-start justify-between p-2">
+                                    <h3 className="text-lg font-medium"></h3>
+                                    <button
+                                        className="p-1 ml-auto bg-transparent border-0 text-black float-right leading-none font-semibold outline-none focus:outline-none"
+                                        onClick={closeDeleteModal}
+                                    >
+                                        <Image
+                                            src="/img/close.svg"
+                                            alt="close"
+                                            className="h-6"
+                                            height={24}
+                                            width={24}
+                                        />
+                                    </button>
+                                </div>
+                                {/*body*/}
+                                <div className="relative pt-2 pb-8 flex-auto">
+                                    <div className="flex justify-start items-center flex-wrap flex-col">
+                                        <p className="flex justify-center items-center text-lg">Are you sure want to <span className="text-[#EF0000] mx-1 font-semibold">Delete</span> this class?</p>
+                                        <div className="mt-10 relative flex justify-center items-center w-full">
+                                            <button
+                                                className="border border-black rounded-lg bg-black text-white text-lg w-[70px] h-[47px] mr-5 hover:bg-yellow-951 hover:text-white hover:border-yellow-951 ease-in-out duration-300 disabled:bg-gray-951 disabled:hover:border-gray-951 disabled:border-gray-951"
+                                                onClick={closeDeleteModal}
+                                            >
+                                                Yes
+                                            </button>
+                                            <button
+                                                className="border border-black rounded-lg bg-white text-black text-lg w-[70px] h-[47px] hover:text-white hover:bg-yellow-951 hover:border-yellow-951 ease-in-out duration-300"
+                                                onClick={closeDeleteModal}
+                                            >
+                                                No
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="opacity-75 fixed inset-0 z-40 bg-black"></div>
+                </>
+            }
+
         </div>
     )
 }
--- src/pages/dashboard/assetmanagement/objectmanagement.tsx
+++ src/pages/dashboard/assetmanagement/objectmanagement.tsx
@@ -17,10 +17,7 @@ export default function ObjectManagement(props: any) {
     const [actions, setActions] = useState(false);
     const [actionCount, setActionCount] = useState(1);
     const [objID, setObjID] = useState("");
-    // const getSelClass = useSelector((state:any) => state.classReducer)
-    // console.log({
-    //     getSelClass:getSelClass.selectedClass
-    // })
+    const [deleteModal, setDeleteModal] = useState(false);
  
     const toggleFilterFunction = () => {
         setToggleArrow(!toggleArrow);
@@ -82,6 +79,17 @@ export default function ObjectManagement(props: any) {
         setObjID(item);
         props.handelObject(item)
     }
+
+
+    const deleteModalFunction = () => {
+        setDeleteModal(true);
+        setActions(false);
+    }
+
+    const closeDeleteModal = () => {
+        setDeleteModal(false)
+    }
+
     return (
         <div className='py-3 font-OpenSans'>
             {/* Title, search and filters */}
@@ -190,11 +198,11 @@ export default function ObjectManagement(props: any) {
                                                 className="text-white text-[14px] hover:bg-yellow-951 hover:text-black h-[40px] px-4 border-b border-gray-900 w-full text-left flex items-center justify-start">
                                                 <span>Edit</span>
                                             </Link>
-                                            <Link
-                                                href="#"
+                                            <button
+                                                onClick={deleteModalFunction}
                                                 className="text-white text-[14px] hover:bg-yellow-951 hover:text-black h-[40px] px-4 border-b border-gray-900 w-full text-left flex items-center justify-start">
                                                 <span>Delete</span>
-                                            </Link>
+                                            </button>
                                             <Link
                                                 href="#"
                                                 className="text-white text-[14px] hover:bg-yellow-951 hover:text-black h-[40px] px-4 border-b border-gray-900 w-full text-left flex items-center justify-start">
@@ -270,6 +278,61 @@ export default function ObjectManagement(props: any) {
                     </tbody>
                 </table>
             </div>
+
+
+            {/* Delete Modal */}
+            {deleteModal &&
+                <>
+                    <div
+                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                    >
+                        <div className="relative my-6 w-[580px]">
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                {/*header*/}
+                                <div className="flex items-start justify-between p-2">
+                                    <h3 className="text-lg font-medium"></h3>
+                                    <button
+                                        className="p-1 ml-auto bg-transparent border-0 text-black float-right leading-none font-semibold outline-none focus:outline-none"
+                                        onClick={closeDeleteModal}
+                                    >
+                                        <Image
+                                            src="/img/close.svg"
+                                            alt="close"
+                                            className="h-6"
+                                            height={24}
+                                            width={24}
+                                        />
+                                    </button>
+                                </div>
+                                {/*body*/}
+                                <div className="relative pt-2 pb-8 flex-auto">
+                                    <div className="flex justify-start items-center flex-wrap flex-col">
+                                        <p className="flex justify-center items-center text-lg">Are you sure want to <span className="text-[#EF0000] mx-1 font-semibold">Delete</span> this object?</p>
+                                        <div className="mt-10 relative flex justify-center items-center w-full">
+                                            <button
+                                                className="border border-black rounded-lg bg-black text-white text-lg w-[70px] h-[47px] mr-5 hover:bg-yellow-951 hover:text-white hover:border-yellow-951 ease-in-out duration-300 disabled:bg-gray-951 disabled:hover:border-gray-951 disabled:border-gray-951"
+                                                onClick={closeDeleteModal}
+                                            >
+                                                Yes
+                                            </button>
+                                            <button
+                                                className="border border-black rounded-lg bg-white text-black text-lg w-[70px] h-[47px] hover:text-white hover:bg-yellow-951 hover:border-yellow-951 ease-in-out duration-300"
+                                                onClick={closeDeleteModal}
+                                            >
+                                                No
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="opacity-75 fixed inset-0 z-40 bg-black"></div>
+                </>
+            }
+
+
+
         </div>
     )
 }
