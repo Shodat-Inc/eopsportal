1905fdeb49ebad075688e7874b0341d69ce69bb9
Kapil <kapilsharma@shodat.com>
2023-12-20 18:58:30 +0530

    transaction on users

--- src/helpers/api/repo/company-record-repo.ts
+++ src/helpers/api/repo/company-record-repo.ts
@@ -14,7 +14,7 @@ export const CompanyRecordRepo = {
  * @param {Object} params - The parameters containing information to save the company record.
  * @returns {Object} - Response object indicating the success or failure of the operation.
  */
-async function create(params: any) {
+async function create(params: any, transaction: any) {
   // Log the initiation of a company record creation.
   loggerInfo.info("POST api/createUsers Save Company records");
 
@@ -22,7 +22,7 @@ async function create(params: any) {
     // Validate that the userId is provided.
     if (params.userId) {
       // Retrieve the user by its primary key (ID) from the database.
-      const user = await db.User.findByPk(params.userId);
+      const user = await db.User.findByPk(params.userId, { transaction });
 
       // If no user is found with the given ID, return an error response.
       if (!user) {
@@ -34,10 +34,10 @@ async function create(params: any) {
     }
 
     // Create a new companyRecord instance using the provided parameters.
-    const companyRecord = new db.companyRecord(params);
+    const companyRecord = new db.companyRecord(params, { transaction });
 
     // Save the new company record to the database.
-    const data = await companyRecord.save();
+    const data = await companyRecord.save({ transaction });
 
     // Return a successful response indicating the company record was added.
     return sendResponseData(true, message.success.companyRecordAdded, data);
