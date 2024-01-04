63eca235edaf0e90b41f4e943c027f9567ddd406
amitshodat <amitpandey@shodat.com>
2024-01-04 20:41:14 +0530

    Implemented Delete API for Classes

--- src/pages/dashboard/assetmanagement/classmanagement.tsx
+++ src/pages/dashboard/assetmanagement/classmanagement.tsx
@@ -7,6 +7,7 @@ import AddNewClass from './addnewclass';
 import { successMessageAction } from '@/store/actions/classAction';
 import EditClass from './editclass';
 import { editClassModalAction } from '@/store/actions/classAction';
+import axios from 'axios';
 
 export default function ClassManagement(props: any) {
     const dispatch = useDispatch<any>();
@@ -19,7 +20,13 @@ export default function ClassManagement(props: any) {
     const [deleteModal, setDeleteModal] = useState(false);
     const [searchClass, setSearchClass] = useState('');
     const [allData, setAllData] = useState(props.classData);
-    const [selectedClass, setSelectedClass] = useState("")
+    const [selectedClass, setSelectedClass] = useState("");
+    const [deleteID, setDeleteID] = useState(0);
+    const [deleteMessage, setDeleteMessage] = useState(false);
+    let access_token = "" as any;
+    if (typeof window !== 'undefined') {
+        access_token = localStorage.getItem('authToken')
+    }
 
     // All class reducer states
     const allClassSelector = useSelector((state: any) => state.classReducer);
@@ -67,7 +74,8 @@ export default function ClassManagement(props: any) {
     //     setShowEditClassModal(false)
     // }
 
-    const deleteModalFunction = () => {
+    const deleteModalFunction = (id: any) => {
+        setDeleteID(id);
         setDeleteModal(true);
         setActions(false);
     }
@@ -104,11 +112,47 @@ export default function ClassManagement(props: any) {
 
 
     // Open Edit class modal
-    const openEditClassModal = (item:any) => {
+    const openEditClassModal = (item: any) => {
         setSelectedClass(item)
         dispatch(editClassModalAction(true));
         setActions(false);
     }
+
+
+    // CALLLING DELETE ASSET API WHEN CONFIRM 'YES' BUTTON CLICKED!
+    const confirmDeleteClass = async (index: any) => {
+        setDeleteModal(false);
+        try {
+            await axios({
+                method: 'DELETE',
+                // url: `http://20.232.178.134:3000/api/deleteClasses?id=${index}`,
+                url: `/api/deleteClasses?id=${index}`,
+                headers: {
+                    "Authorization": `Bearer ${access_token}`,
+                    "Content-Type": "application/json"
+                }
+            }).then(function (response) {
+                console.log({
+                    response: response,
+                    message: "Class deleted successful!!"
+                })
+                setDeleteMessage(true);
+                setTimeout(() => {
+                    setDeleteMessage(false)
+                }, 2000)
+            }).catch(function (error) {
+                console.log({
+                    "ERROR IN AXIOS CATCH (DELETE)": error
+                })
+            })
+        } catch (err) {
+            console.log({
+                "ERROR IN TRY CATCH (DELETE)": err
+            })
+        }
+
+    }
+
 
     return (
         <div className='px-0 py-3 font-OpenSans'>
@@ -177,7 +221,22 @@ export default function ClassManagement(props: any) {
                 </div>
             }
 
-
+            {/* Success / Error Message */}
+            <div className='flex justify-start items-center px-4'>
+                {deleteMessage &&
+                    <div className={`bg-blue-957 border-blue-958 text-blue-959 mb-1 mt-1 border text-md px-4 py-3 rounded rounded-xl relative flex items-center justify-start`}>
+                        <Image
+                            src="/img/AlertInfo.svg"
+                            alt="Alert Success"
+                            height={24}
+                            width={24}
+                            className='mr-2'
+                        />
+                        <strong className="font-semibold">Success</strong>
+                        <span className="block sm:inline ml-2">Class has been deleted successfully!</span>
+                    </div>
+                }
+            </div>
 
             {/* Table */}
             <div className='w-full mt-6 min-h-[400px]'>
@@ -207,7 +266,7 @@ export default function ClassManagement(props: any) {
                                                 <button
                                                     onClick={() => takeMeToClassComponent(item.assetName)}
                                                 >
-                                                    <span>{item.className}</span>
+                                                    <span>{item.id} - {item.className}</span>
                                                 </button>
                                             </td>
                                             <td>
@@ -235,12 +294,12 @@ export default function ClassManagement(props: any) {
                                                     {(actions && actionCount === index + 1) &&
                                                         <div className="bg-black text-white border overflow-hidden border-black rounded rounded-xl w-[100px] flex flex-col flex-wrap items-start justify-start shadow-sm absolute top-[30px] right-[75px] z-[1]">
                                                             <button
-                                                                onClick={()=>openEditClassModal(item.assetName)}
+                                                                onClick={() => openEditClassModal(item.assetName)}
                                                                 className="text-white text-[14px] hover:bg-yellow-951 hover:text-black h-[30px] px-4 border-b border-gray-900 w-full text-left flex items-center justify-start">
                                                                 <span>Edit</span>
                                                             </button>
                                                             <button
-                                                                onClick={deleteModalFunction}
+                                                                onClick={() => deleteModalFunction(item.id)}
                                                                 className="text-white text-[14px] hover:bg-yellow-951 hover:text-black h-[30px] px-4 border-b border-gray-900 w-full text-left flex items-center justify-start">
                                                                 <span>Delete</span>
                                                             </button>
@@ -316,7 +375,7 @@ export default function ClassManagement(props: any) {
                                         <div className="mt-10 relative flex justify-center items-center w-full">
                                             <button
                                                 className="border border-black rounded-lg bg-black text-white text-lg w-[70px] h-[47px] mr-5 hover:bg-yellow-951 hover:text-white hover:border-yellow-951 ease-in-out duration-[100ms] disabled:bg-gray-951 disabled:hover:border-gray-951 disabled:border-gray-951"
-                                                onClick={closeDeleteModal}
+                                                onClick={() => confirmDeleteClass(deleteID)}
                                             >
                                                 Yes
                                             </button>
