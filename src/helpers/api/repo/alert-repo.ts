f3ff4a176760efcdea8f155d7869922b2ff8cb98
Kapil <kapilsharma@shodat.com>
2024-01-23 11:25:00 +0530

    Changes in Alert Repo

--- src/helpers/api/repo/alert-repo.ts
+++ src/helpers/api/repo/alert-repo.ts
@@ -5,116 +5,123 @@ import message from "@/util/responseMessage";
 import { send } from "process";
 
 export const alertRepo = {
-    create,
-    getAllAlert,
-    getAlertById,
-    update,
-    delete: _delete
+  create,
+  getAllAlert,
+  getAlertById,
+  update,
+  delete: _delete,
 };
 
 async function create(params: any) {
-    loggerInfo.info("Post api/createAlerts to create Alerts")
+  loggerInfo.info("Post api/createAlerts to create Alerts");
 
-    try {
-        const data = await db.Alert.findOne({
-            where: {
-                alertName: params.alertName
-            }
-        });
+  try {
+    const data = await db.Alert.findOne({
+      where: {
+        alertName: params.alertName,
+      },
+    });
 
-        if (data) {
-            return sendResponseData(false, "Alert Name already Exists", {});
-        }
-
-        const newAlert = new db.Alert(params);
-
-        const save = await newAlert.save();
-
-        // Return a successful response with the saved data
-        return sendResponseData(true, "Alert Created Successfully", save);
-
-    } catch (error) {
-        loggerError.error("Error in Alert Repo", error)
-        return sendResponseData(false, "Error in Alert Repo", error)
+    if (data) {
+      return sendResponseData(false, "Alert Name already Exists", {});
     }
+
+    const newAlert = new db.Alert(params);
+
+    const save = await newAlert.save();
+
+    // Return a successful response with the saved data
+    return sendResponseData(true, "Alert Created Successfully", save);
+  } catch (error) {
+    loggerError.error("Error in Alert Repo", error);
+    return sendResponseData(false, "Error in Alert Repo", error);
+  }
 }
 
 async function getAllAlert() {
-    loggerInfo.info("Get all Alerts")
-    try {
-        const data = await db.Alert.findAll({
-            attributes: ['alertName', 'thresholdValue', 'enable']
-        })
-        if (!data) {
-            return sendResponseData(false, "Data doesn't Exists", {})
-        }
-        return sendResponseData(true, "Alert Data fetched successfully", data)
-    } catch (error) {
-        loggerError.error("Error in getting all the alerts", error)
-        return sendResponseData(false, "Error in getting all the alerts", error)
+  loggerInfo.info("Get all Alerts");
+  try {
+    const data = await db.Alert.findAll({
+      attributes: [
+        "id",
+        "alertName",
+        "thresholdValue",
+        "enable",
+        "createdAt",
+        "updatedAt",
+      ],
+    });
+    if (!data) {
+      return sendResponseData(false, "Data doesn't Exists", {});
     }
+    return sendResponseData(true, "Alert Data fetched successfully", data);
+  } catch (error) {
+    loggerError.error("Error in getting all the alerts", error);
+    return sendResponseData(false, "Error in getting all the alerts", error);
+  }
 }
 
 async function getAlertById(params: any) {
-    loggerInfo.info("Get ALert Data By Id")
-    try {
-        console.log(params)
-        if (!params.id) {
-            return sendResponseData(false, "Data is not provided in params", {})
-        }
-        const data = await db.Alert.findOne({
-            where: { id: params.id }
-        })
-        if (!data) {
-            return sendResponseData(false, "Data doesn't Exists", {})
-        }
-        return sendResponseData(true, "Data fetched successfully", data)
-    } catch (error) {
-        loggerError.error("Error in Alert Repo", error)
-        return sendResponseData(false, "Error in Alert Repo", error)
+  loggerInfo.info("Get ALert Data By Id");
+  try {
+    console.log(params);
+    if (!params.id) {
+      return sendResponseData(false, "Data is not provided in params", {});
     }
+    const data = await db.Alert.findOne({
+      where: { id: params.id },
+    });
+    if (!data) {
+      return sendResponseData(false, "Data doesn't Exists", {});
+    }
+    return sendResponseData(true, "Data fetched successfully", data);
+  } catch (error) {
+    loggerError.error("Error in Alert Repo", error);
+    return sendResponseData(false, "Error in Alert Repo", error);
+  }
 }
 
 async function update(params: any, reqAuth: any) {
-    loggerInfo.info("Update Alert")
-    try {
-        const data = await db.Alert.findOne({
-            where: { id: reqAuth.sub }
-        });
+  loggerInfo.info("Update Alert");
+  try {
+    const data = await db.Alert.findOne({
+      where: { id: reqAuth.sub },
+    });
 
-        // Define the properties to update
-        const propertiesToUpdate = [
-            "alertName",
-            "thresholdValue",
-            "enable",
-            "emailSubject",
-            "emailContent"
-        ];
+    // Define the properties to update
+    const propertiesToUpdate = [
+      "alertName",
+      "thresholdValue",
+      "enable",
+      "emailSubject",
+      "emailContent",
+    ];
 
-        propertiesToUpdate.forEach((property) => {
-            if (params[property]) {
-                data[property] = params[property];
-            }
-        });
+    propertiesToUpdate.forEach((property) => {
+      if (params[property]) {
+        data[property] = params[property];
+      }
+    });
 
-        const response = await data.save();
+    const response = await data.save();
 
-        if (!data) {
-            return sendResponseData(false, "Data doesn't exists", []);
-        }
-
-        // Return a successful response with the updated enterprise data
-        return sendResponseData(true, "Data Updated Successfully", response);
-    } catch (error) {
-        loggerError.error("Error in Alert", error)
-        return sendResponseData(false, "Error in Alert Repo", error)
+    if (!data) {
+      return sendResponseData(false, "Data doesn't exists", []);
     }
+
+    // Return a successful response with the updated enterprise data
+    return sendResponseData(true, "Data Updated Successfully", response);
+  } catch (error) {
+    loggerError.error("Error in Alert", error);
+    return sendResponseData(false, "Error in Alert Repo", error);
+  }
 }
 
 async function _delete(id: number) {
-    const alert = await db.Alert.findByPk(id);
-    if (!alert) {
-        return sendResponseData(false, "Alert not found", {})
-    }
-    return await alert.destroy();
+  const alert = await db.Alert.findByPk(id);
+  if (!alert) {
+    return sendResponseData(false, "Alert not found", {});
+  }
+  const response = await alert.destroy();
+  return sendResponseData(true, "Alert Deleted Successfuly", response);
 }
